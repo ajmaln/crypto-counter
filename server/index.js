58da85be7b3d506ee9314739b003aced9df24c08
Joshua <mrjoshuaali@yahoo.co.uk>
2020-08-03 01:31:30 +0100

    Added mongoose, passport, validator

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
